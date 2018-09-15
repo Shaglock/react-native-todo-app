d66b17a6ff524d42852e3d69b481c816cfc18629
Ilya Shaplyko <goldilb10xa@mail.ru>
2018-09-15 17:10:50 +0300

    Add handlers for adding items and toggling complete status of all items

--- App.js
+++ App.js
@@ -4,10 +4,56 @@ import Header from "./components/Header";
 import Footer from "./components/Footer"
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      items: []
+    }
+
+    this.handleAddItem = this.handleAddItem.bind(this)
+    this.handleToggleAllComplete = this.handleToggleAllComplete.bind(this)
+  }
+
+  handleAddItem() {
+    if (!this.state.value) return;
+    const newItems = [
+      ...this.state.items,
+      {
+        key: Date.now(),
+        text: this.state.value,
+        complete: false
+      }
+    ]
+    this.setState({
+      allComplete: false,
+      items: newItems,
+      value: ""
+    })
+  }
+
+  handleToggleAllComplete() {
+    const complete = !this.state.allComplete;
+    const newItems = this.state.items.map((item) => ({
+      ...item,
+      complete
+    }));
+    console.table(newItems);
+    this.setState({
+      items: newItems,
+      allComplete: complete
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Header />
+        <Header
+          value={this.state.value}
+          onAddItem={this.handleAddItem}
+          onChange={(value) => this.setState({ value })}
+          onToggleAllComplete={this.handleToggleAllComplete}
+        />
         <View style={styles.content}>
           <Text>Basic ReactNative ToDo App!</Text>
         </View>
--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, StyleSheet, View, TextInput, TouchableOpacity } from 'react-native'
 
 export default class Header extends Component {
   render() {
     return (
-      <View>
-        <Text> Header </Text>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={this.props.onToggleAllComplete}>
+          <Text
+            style={styles.checkMark}>
+            {String.fromCharCode(10003)}
+          </Text>
+        </TouchableOpacity>
+        <TextInput
+          style={styles.input}
+          value={this.props.value}
+          placeholder="What needs to be done?"
+          blurOnSubmit={false}
+          returnKeyType="done"
+          onChangeText={this.props.onChange}
+          onSubmitEditing={this.props.onAddItem}
+        />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  input: {
+    flex: 1,
+    height: 50,
+    marginLeft: 16
+  },
+  checkMark: {
+    fontSize: 30,
+    color: "#CCC"
+  },
+  header: {
+    paddingHorizontal: 16,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center"
+  }
+})
